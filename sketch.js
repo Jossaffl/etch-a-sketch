b3f2fa40de84d002a0ae8283caa9d0383055e736
Jossaffl <116684778+Jossaffl@users.noreply.github.com>
2023-02-22 14:02:52 -0500

    base logic of project done, missing still some design

--- sketch.js
+++ sketch.js
@@ -1,16 +1,49 @@
-div_container = document.getElementById("container")
+const divContainer = document.getElementById('container');
+const button = document.getElementById('options');
+const resetter = document.getElementById('resetter');
+const boxes = document.getElementsByClassName('untraced');
+const eraser = document.getElementById('eraser');
+const pencil = document.getElementById('pencil');
 
-for(i = 0; i <= 528; ++i){
-    const split_div = document.createElement("div");
-    split_div.classList.add("untraced");
-    div_container.appendChild(split_div);
-
-    split_div.addEventListener("mouseover", (e)=>{
-            split_div.setAttribute("style","backGround-color:black;")
-        })
-    
-    
+function GridResolution(resolution) {
+  for (i = 0; i < resolution * resolution; ++i) {
+    const splitDiv = document.createElement('div');
+    splitDiv.classList.add('untraced');
+    splitDiv.setAttribute('style', `height:${Math.sqrt(262144 / (resolution * resolution))}px; width: ${Math.sqrt(262144 / (resolution * resolution))}px;`);
+    divContainer.appendChild(splitDiv);
+  }
 }
 
+button.addEventListener('click', (e) => {
+  // eslint-disable-next-line no-alert
+  const question = prompt('Please select the number of squares to work with');
+  GridResolution(question);
+});
 
+resetter.addEventListener('click', () => {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < boxes.length; ++i) {
+    boxes[i].setAttribute('style', 'backGround-color:white;');
+  }
+});
 
+function pen() {
+  divContainer.querySelectorAll('div').forEach((element) => {
+    element.addEventListener('mouseover', () => {
+      element.classList.remove('untraced');
+      element.classList.add('traced');
+    });
+  });
+}
+
+function erased() {
+  divContainer.querySelectorAll('div').forEach((element) => {
+    element.addEventListener('mouseover', () => {
+      element.classList.remove('traced');
+      element.classList.add('untraced');
+    });
+  });
+}
+
+pencil.addEventListener('click', pen);
+eraser.addEventListener('click', erased);
